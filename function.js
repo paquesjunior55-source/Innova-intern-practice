686076180d1ac4ceb3e9565914da9139b8865c44
paquesjunior55-source <paquesjunior55@gmail.com>
2026-07-23 10:51:52 +0100

    git commit -m "Add biggestNumber function"

--- function.js
+++ function.js
@@ -7,3 +7,15 @@ return "Odd";
 }
 }
 console,log(checkEven(4));
+
+//find the biggest of three numbers
+function biggestNumber(a,b,c){
+  if (a>b && a>c) {
+    return a;
+  } elseif  (b>a && B>c){
+    return b;
+  } else {
+    return c;
+  }
+}
+console.log(bigestNumber(5,10,3));
